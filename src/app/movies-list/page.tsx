6ae66843934a0a288133f9bcbcc41763009d7d5e
Nikhil <34055828+fullstackdevloper@users.noreply.github.com>
2024-07-10 18:50:34 +0530

    pagination updated

--- src/app/movies-list/page.tsx
+++ src/app/movies-list/page.tsx
@@ -16,6 +16,8 @@ interface Movie {
   publishingYear: number;
 }
 
+type PaginationProps = {currentPage:number;totalPages:number;onPageChange:(page: number) =>void};
+
 const MovieList: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState(true);
@@ -149,42 +151,48 @@ const MovieList: React.FC = () => {
   );
 };
 
-const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  const pages = [];
+const
+  Pagination
+    :
+    React
+    .FC<
+      PaginationProps
+    > =
+    ({ currentPage, totalPages, onPageChange }) => {
+      const pages = [];
 
-  for (let i = 1; i <= totalPages; i++) {
-    pages.push(i);
-  }
+      for (let i = 1; i <= totalPages; i++) {
+        pages.push(i);
+      }
 
-  return (
-    <div className="flex justify-center mt-4">
-      <button
-        className="px-4 py-2 mx-1 bg-gray-200 rounded disabled:opacity-50"
-        onClick={() => onPageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        Previous
-      </button>
-      {pages.map((page) => (
-        <button
-          key={page}
-          className={`px-4 py-2 mx-1 rounded ${
-            page === currentPage ? "bg-blue-500 text-white" : "bg-gray-200"
-          }`}
-          onClick={() => onPageChange(page)}
-        >
-          {page}
-        </button>
-      ))}
-      <button
-        className="px-4 py-2 mx-1 bg-gray-200 rounded disabled:opacity-50"
-        onClick={() => onPageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-      >
-        Next
-      </button>
-    </div>
-  );
-};
+      return (
+        <div className="flex justify-center mt-4">
+          <button
+            className="px-4 py-2 mx-1 bg-gray-200 rounded disabled:opacity-50"
+            onClick={() => onPageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          {pages.map((page) => (
+            <button
+              key={page}
+              className={`px-4 py-2 mx-1 rounded ${page === currentPage ? "bg-blue-500 text-white" : "bg-gray-200"
+                }`}
+              onClick={() => onPageChange(page)}
+            >
+              {page}
+            </button>
+          ))}
+          <button
+            className="px-4 py-2 mx-1 bg-gray-200 rounded disabled:opacity-50"
+            onClick={() => onPageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
+        </div>
+      );
+    };
 
 export default MovieList;
